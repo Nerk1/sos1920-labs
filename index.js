87aa7abee85f3f63e339423831ffb341e3666026
Nerk1 <nerk1toran@gmail.com>
2020-03-09 13:04:53 +0000

    aaa

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cool = require("cool-ascii-faces");
 const express = require("express");
 
 var app = express();
+app.use("/", express.static("./public"));
 
 app.get("/cool", (request, response) => {
 	response.send("<html>"+cool()+"</html>");
